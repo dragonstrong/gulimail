332471c72a6d9b30abe31bf91155798ccddd3f84
qiang.long <Long_Q@outlook.com>
2024-03-16 13:13:41 +0800

    项目初始化

---
+++ gulimall-coupon/src/main/java/com/atguigu/guliamll/coupon/GulimallCouponApplication.java
@@ -0,0 +1,9 @@
+package com.atguigu.guliamll.coupon;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class GulimallCouponApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+}
